a67cebdf50941d50fd6b505fd5004d31a5b0dfea
Marina Sidorkina <m.sidorkina55@gmail.com>
2019-03-01 01:17:48 +0300

    Fix random number generating function

--- src/utils.js
+++ src/utils.js
@@ -1,1 +1,1 @@
-export const generateRandomNumber = (min, max) => Math.floor(Math.random() * (max - min) + min);
+export const generateRandomNumber = (min, max) => Math.floor(Math.random() * (max + 1 - min) + min);
