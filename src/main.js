f73d3dc289e6223becb16c82acaf82e08c62eeba
Marina Sidorkina <m.sidorkina55@gmail.com>
2019-03-01 11:16:42 +0300

    Change object to variables.

--- src/main.js
+++ src/main.js
@@ -3,10 +3,8 @@ import {generateCards} from './mocks/cards.js';
 import {createCardsTemplate} from './templates/cards.js';
 import {createFilterTemplate} from './templates/filter.js';
 
-const FilmsAmountLimit = {
-  MIN: 2,
-  MAX: 7
-};
+const CARDS_LIMIT_MIN = 2;
+const CARDS_LIMIT_MAX = 7;
 
 const filmsMainElement = document.querySelector(`.films-list .films-list__container`);
 const filmsTopRatedElement = document.querySelector(`.films-list--extra:nth-child(2) .films-list__container`);
@@ -16,15 +14,15 @@ const filterElement = document.querySelector(`.main-navigation`);
 const addFilterClickEventListener = () => {
   document.querySelectorAll(`.main-navigation__item:not(.main-navigation__item--additional)`).forEach((element) => {
     element.addEventListener(`click`, () => {
-      filmsMainElement.innerHTML = createCardsTemplate(generateCards(generateRandomNumber(FilmsAmountLimit.MIN, FilmsAmountLimit.MAX)));
+      filmsMainElement.innerHTML = createCardsTemplate(generateCards(generateRandomNumber(CARDS_LIMIT_MIN, CARDS_LIMIT_MAX)));
     });
   });
 };
 
-filmsMainElement.innerHTML = createCardsTemplate(generateCards(FilmsAmountLimit.MAX));
-filmsTopRatedElement.innerHTML = createCardsTemplate(generateCards(FilmsAmountLimit.MIN),
+filmsMainElement.innerHTML = createCardsTemplate(generateCards(CARDS_LIMIT_MAX));
+filmsTopRatedElement.innerHTML = createCardsTemplate(generateCards(CARDS_LIMIT_MIN),
     {description: false, controls: false});
-filmsMostCommentedElement.innerHTML = createCardsTemplate(generateCards(FilmsAmountLimit.MIN),
+filmsMostCommentedElement.innerHTML = createCardsTemplate(generateCards(CARDS_LIMIT_MIN),
     {description: false, controls: false});
 filterElement.innerHTML = createFilterTemplate();
 
